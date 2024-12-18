e6e77c0ba8f308d70dc381d1b137d2b4feebd2aa
kfijalkowski1 <k.1.fijalkowski@gmail.com>
2024-12-18 01:40:48 +0100

    fix api port

--- src/componenets/utils/consts.ts
+++ src/componenets/utils/consts.ts
@@ -1,4 +1,4 @@
-// export const API_ADDR = 'http://49.13.73.66:8080/';
-export const API_ADDR = 'http://127.0.0.1:8000/';
+export const API_ADDR = 'http://49.13.73.66:8080/';
+// export const API_ADDR = 'http://127.0.0.1:8000/';
 
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
