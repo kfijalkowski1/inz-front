09be76e752cb3d8e0e9783c81749e8d05c4398fe
kfijalkowski1 <k.1.fijalkowski@gmail.com>
2024-12-23 12:32:57 +0100

    Change status to stan zgłoszenia

--- src/componenets/requests/single/RequestInfo.tsx
+++ src/componenets/requests/single/RequestInfo.tsx
@@ -11,7 +11,7 @@ export function RequestInfo(props: { request: RequestType, authorName: string })
             Autor: {props.authorName}
         </h6>
         <h6 className="font-extralight tracking-tight text-gray-800 dark:text-white">
-            Status: {props.request?.status}
+            Stan zgłoszenia: {props.request?.status}
         </h6>
         <p className="font-normal text-gray-700 dark:text-gray-400">
             {props.request?.description}
